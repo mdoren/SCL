03879062b87d6b8711f230d376a6b98315f37f7c
Matt Jason Doren <mdoren@e104-mb-03.mchenry.edu>
2020-01-29 10:53:15 -0600

    Initial commit

---
+++ src/Operators.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var a = 12
+    var b = 4
+
+    println("This comparison should say true")
+    var isCompare = a > b
+    println(isCompare)
+
+    var ourAnswer = 0
+    ourAnswer = ourAnswer + 1 //1
+    print(ourAnswer)
+
+    ourAnswer += 5  //6
+    println( ourAnswer)
+    ourAnswer++
+    println( ourAnswer)
+}
